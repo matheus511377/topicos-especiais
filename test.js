6172939becd354b26bb3539f08cb1fe3f6f49815
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-09-01 13:55:15 -0300

    chore: form logic

--- test.js
+++ test.js
@@ -1,1 +1,29 @@
-console.log('Hello World!');
+const input_ids = [ 'name', 'birth_date', 'email', 'password','genre','phone','cellphone','smoker','town']
+
+window.onload=function(){
+  const element = document.querySelector('form');
+
+  element.addEventListener('submit', event => {
+    event.preventDefault();
+    setFormValues();
+  });
+}
+
+const setFormValues = () => { 
+  for(let input_id of input_ids){
+    document.getElementById(`persisted_${input_id}`).value = document.getElementById(input_id).value;
+  }
+}
+
+const cleanFormValues = () => {
+  for(let input_id of input_ids){
+    document.getElementById(input_id).value = '';
+  }
+}
+
+const cleanPersistedValues = () => {
+  for(let input_id of input_ids){
+    document.getElementById(`persisted_${input_id}`).value = '';
+  }
+  cleanFormValues();
+}
